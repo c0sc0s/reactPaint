67563dd8995be1ec2aebcd1eea4a22f3edc2ae32
c0sc0s <yuwenjieshuai@gmail.com>
2023-04-16 21:31:03 +0800

    build files: add userState

---
+++ client/src/store/userState.ts
@@ -0,0 +1,24 @@
+import { UserType } from "@/types/user";
+import { makeAutoObservable } from "mobx";
+
+class UserState {
+  users: UserType[] = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  public setUsers(users: UserType[]) {
+    this.users = users;
+  }
+
+  public addUser(user: UserType) {
+    this.users.push(user);
+  }
+
+  public removeUser(user: UserType) {
+    this.users = this.users.filter((u) => u.id !== user.id);
+  }
+}
+
+export default new UserState();
