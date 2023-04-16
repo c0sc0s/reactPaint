2f117819158cfcb26c40ef7e21f71be2774331a8
c0sc0s <yuwenjieshuai@gmail.com>
2023-04-17 00:12:47 +0800

    build: tools 出口

---
+++ client/src/tool/index.ts
@@ -0,0 +1,7 @@
+import Brush from "./main/Brush";
+import Circle from "./main/Circle";
+import Eraser from "./main/Eraser";
+import Line from "./main/Line";
+import Rect from "./main/Rect";
+
+export { Brush, Circle, Eraser, Line, Rect };
