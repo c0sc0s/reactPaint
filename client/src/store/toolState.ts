52ecff2a37c1a686652f409e98fc78a382fc9960
c0sc0s <yuwenjieshuai@gmail.com>
2023-04-16 22:24:06 +0800

    toolState

--- client/src/store/toolState.ts
+++ client/src/store/toolState.ts
@@ -1,7 +1,7 @@
 import Tool from "@/tool/main/Tool";
 import { makeAutoObservable } from "mobx";
 
-class TooState {
+class ToolState {
   tool: Tool | null = null;
   toolScaleFactor = 1;
   strokeColor = "#000";
@@ -20,7 +20,46 @@ class TooState {
   public setFillColor(color: string) {
     if (this.tool) {
       this.fillColor = color;
-      this.tool.fillColor = color;
+      this.tool.fillColor = color; //proxy
     }
   }
+
+  public setStrokeColor(color: string) {
+    if (this.tool) {
+      this.strokeColor = color;
+      this.tool.strokeColor = color; //proxy
+    }
+  }
+
+  public setLineWidth(width: number) {
+    if (this.tool) {
+      this.lineWidth = width;
+      this.tool.lineWidth = width; //proxy
+    }
+  }
+
+  public setToolScaleFactor(scaleFactor: number) {
+    this.toolScaleFactor = scaleFactor;
+  }
+
+  public setCachedScaleFactor(scaleFactor: number) {
+    this.cachedScaleFactor = scaleFactor;
+  }
+
+  get currentToolName() {
+    return this.tool?.name;
+  }
+
+  get currentFillColor() {
+    return this.fillColor;
+  }
+
+  get currentStrokeColor() {
+    return this.strokeColor;
+  }
+
+  get currentLineWidth() {
+    return this.lineWidth;
+  }
 }
+export default new ToolState();
--- client/src/tool/main/Tool.ts
+++ client/src/tool/main/Tool.ts
@@ -18,4 +18,22 @@ export default class Tool {
     if (this.ctx) {
     }
   }
+
+  set fillColor(color: string) {
+    if (this.ctx) {
+      this.ctx.fillStyle = color;
+    }
+  }
+
+  set strokeColor(color: string) {
+    if (this.ctx) {
+      this.ctx.strokeStyle = color;
+    }
+  }
+
+  set lineWidth(width: number) {
+    if (this.ctx) {
+      this.ctx.lineWidth = width;
+    }
+  }
 }
