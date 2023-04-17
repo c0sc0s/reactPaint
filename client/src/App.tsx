07a29185d225bc45538ab4ba2e3efd78a7889f2b
c0sc0s <yuwenjieshuai@gmail.com>
2023-04-17 14:47:31 +0800

    build: react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import Home from "./pages/Home";
 import "@/scss/main.scss";
-import { toast } from "react-hot-toast";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+const sessionId = `${(+new Date()).toString(16)}`;
 
 const App = () => {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:id" element={<Home />} />
+        <Route path="*" element={<Navigate to={sessionId} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
